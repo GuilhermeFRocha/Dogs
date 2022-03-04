6346709c67c37b0c532d52d682f937f589747848
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-03-04 14:15:22 -0300

    page login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-import './App.css';
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login/*" element={<Login />} />
+        </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
