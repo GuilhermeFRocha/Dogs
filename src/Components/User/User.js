2aafa179a9d0dd897cb565ee2ecd99783c9e11ce
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-05-10 19:37:37 -0300

    scroll infinite

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
 import Feed from '../Feed/Feed'
 import UserHeader from './UserHeader'
 import UserPhotoPost from './UserPhotoPost'
@@ -7,11 +8,14 @@ import UserStats from './UserStats'
 
 
 const User = () => {
+  const {data} = React.useContext(UserContext)
+
+
   return (
     <section className={"container"}>
       <UserHeader/>
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id}/>} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
       </Routes>
